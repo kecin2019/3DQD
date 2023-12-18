41464d84b5af43b08a97756beaab0273fc21d0cf
Kecin2019 <1139962358@qq.com>
2023-12-18 16:41:50 +0800

    jkw

--- launchers/train_pvqvae_snet.sh
+++ launchers/train_pvqvae_snet.sh
@@ -48,4 +48,4 @@ python train.py --name ${name} --gpu_ids ${gpuid} --lr ${lr} --batch_size ${batc
                 --nepochs ${nepochs} --nepochs_decay ${nepochs_decay} \
                 --dataset_mode ${dataset_mode} --cat ${cat} --trunc_thres ${trunc_thres} --max_dataset_size ${max_dataset_size} \
                 --display_freq ${display_freq} --print_freq ${print_freq} \
-                 --debug ${debug}
+                --debug ${debug}
